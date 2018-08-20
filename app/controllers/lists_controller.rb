29ceb1307e0a9e96b8d8aa20e0f0386756afbaab
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-08-20 15:03:03 -0600

    making create list page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,15 @@ class ListsController < ApplicationController
     erb :'/lists/create_list'
   end
 
+  post '/list' do
+    @list = List.new(name: params[:name])
+    @list.save
+
+    ##iterate over task[name] to create all isntance of tasks
+    @tasks = params[:task[name]]
+    @tasks.each do |task|
+      @task = Task.new(name: params[:task[name]], list_id: @list.id)
+      @task.save
+    end
+  end
 end
