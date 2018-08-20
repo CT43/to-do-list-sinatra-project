7ee4390740d60e4292fc92886bcb17d723bef1f2
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-08-20 14:52:07 -0600

    added user signup page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,7 @@
 class ListsController < ApplicationController
+
+  get '/create_list' do
+    erb :'/lists/create_list'
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  get "/signup" do
+    erb :'/users/create_user'
+  end
+
+  get '/show' do
+    @user = User.last
+    erb :'/users/show'
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user.save
+    redirect to '/show'
+  end
+
+
+
+
 end
