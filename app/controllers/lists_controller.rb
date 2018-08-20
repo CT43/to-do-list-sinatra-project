e26d9a4b589a393a7720e184611838e1a066417e
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-08-20 15:45:33 -0600

    building out list creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,23 @@ class ListsController < ApplicationController
 
   post '/list' do
     @list = List.new(name: params[:name])
+    @list.user_id = session[:user_id]
     @list.save
+    binding.pry
 
     ##iterate over task[name] to create all isntance of tasks
-    @tasks = params[:task[name]]
+    @tasks = params[:task][:name]
     @tasks.each do |task|
-      @task = Task.new(name: params[:task[name]], list_id: @list.id)
+      @task = Task.new(name: task)
+      @task.list_id = @list.id
       @task.save
     end
+    binding.pry
+    redirect to '/show'
+  end
+
+  get '/lists/:id' do
+    @list = List.find_by(id: params[:id])
+    erb :'lists/show_list'
   end
 end
