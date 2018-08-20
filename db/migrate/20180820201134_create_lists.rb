71b0e7345e4ee756dd6f83bd3506bedb6e875b6d
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-08-20 14:22:11 -0600

    created tables ran migrations

--- db/migrate/20180820201134_create_lists.rb
+++ db/migrate/20180820201134_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
-      t.string :content
+      t.string :name
       t.integer :user_id
 
       t.timestamps null: false
--- db/migrate/20180820201340_create_tasks.rb
+++ db/migrate/20180820201340_create_tasks.rb
@@ -1,4 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :list_id
+
+      t.timestamps null: false
+    end
   end
 end
