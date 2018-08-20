c532849353f0f0d1f846a2a6f0f20db387e641f1
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-08-20 16:04:03 -0600

    adding authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,27 @@
 class UsersController < ApplicationController
 
-  get "/signup" do
-    erb :'/users/create_user'
+  get '/signup' do
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'/users/create_user'
+    end
   end
 
-  get '/show' do
-    @user = User.last
+  get '/users/:slug' do
+    @user = User.find_by(params[:slug])
     erb :'/users/show'
   end
 
   post '/signup' do
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user.save
-    session[:user_id] = @user.id
-    redirect to '/show'
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/users/#{@user.slug}"
+    end
   end
 
 
